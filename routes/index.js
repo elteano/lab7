316835144e3be1819f80936d20597912b86144ec
Thomas Tucker <elteano@gmail.com>
2016-02-18 11:48:13 -0800

    Added coin toss.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,15 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num > 0.5) {
 		projects['grid'] = false;
-  	res.render('index', projects);
+		res.render('index', projects);
+	} else {
+		res.redirect('/grid');
+	}
 };
 
 exports.viewGrid = function (req, res)
